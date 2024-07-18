89a2cd88c73f68278118c8ff2240b5caf26c10b9
ugwu somto <ugwujude42@gmail.com>
2024-07-18 20:44:04 +0100

    Setting up modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BlogController } from './blog/blog.controller';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule, BlogModule],
+  controllers: [AppController, BlogController],
   providers: [AppService],
 })
 export class AppModule {}
